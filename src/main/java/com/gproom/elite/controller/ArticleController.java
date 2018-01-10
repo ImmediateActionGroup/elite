00c63cff8e2721cf6ca119486f7ee4b19c0ecf87
beishan <312251611@qq.com>
2018-01-10 18:03:38 +0800

    first commit

--- src/main/java/com/gproom/elite/controller/ArticleController.java
+++ src/main/java/com/gproom/elite/controller/ArticleController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api")
 public class ArticleController {
-
     @Autowired
     private ArticleService articleService;
 
