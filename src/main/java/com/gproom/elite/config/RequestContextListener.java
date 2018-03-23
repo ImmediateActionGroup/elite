226a4b6665a61b32f48eec2ddf173d07d8361dd1
xueshan.wei <312251611@qq.com>
2018-03-24 00:03:19 +0800

    添加RequestListener

--- src/main/java/com/gproom/elite/config/RequestContextListener.java
+++ src/main/java/com/gproom/elite/config/RequestContextListener.java
@@ -2,9 +2,11 @@ package com.gproom.elite.config;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletRequestEvent;
 import javax.servlet.ServletRequestListener;
+import javax.servlet.annotation.WebListener;
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -12,6 +14,8 @@ import javax.servlet.http.HttpServletRequest;
  * @Date 2018/3/23 下午10:00
  */
 @Slf4j
+@WebListener
+@Component
 public class RequestContextListener implements ServletRequestListener{
     @Override
     public void requestDestroyed(ServletRequestEvent requestEvent) {
